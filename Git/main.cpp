ee35d937994146e7ae599fec61023ecb728a8ebf
Shannon Smith <shannones2013@gmail.com>
2016-09-01 12:16:08 -0700

    addTwoInts();

--- Git/main.cpp
+++ Git/main.cpp
@@ -7,9 +7,29 @@
 //
 
 #include <iostream>
+using namespace std;
 
+int addTwoInts(int, int);
 int main(int argc, const char * argv[]) {
     // insert code here...
+    int number1;
+    int number2;
+    
+    cout << "enter 1 number\n";
+    cin >> number1;
+    cout << "enter 2 number\n";
+    cin >> number2;
+    
+    
+    cout << number1 << " + " << number2 << " = " << addTwoInts(number1, number2) << endl;
+    
+    
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+
+int addTwoInts( int int1, int int2){
+    return (int1 + int2);
+}//add2
